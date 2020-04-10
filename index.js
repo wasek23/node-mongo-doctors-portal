4ceb69b5ea25c1f3aa9650253f6f5f0bdd5e9b85
wasek23 <wasekbellah2311@gmail.com>
2020-04-10 22:40:29 +0600

    complete update database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 app.use(cors());
@@ -34,6 +35,8 @@ app.get('/appointments', (req, res) => {
 // Post
 app.post('/addAppointment', (req, res) => {
     const appointment = req.body;
+    appointment.status = "pending";
+    appointment.prescription = null;
 
     client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
@@ -46,6 +49,47 @@ app.post('/addAppointment', (req, res) => {
                 res.send(result.ops[0]);
             }
         });
+        client.close();
+    });
+});
+
+app.post('/updatePrescription', (req, res) => {
+    const appointment = req.body;
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("patientsDetails").collection("appointments");
+        collection.updateOne({ _id: ObjectId(appointment.id) }, { $set: { "prescription": appointment.prescription } }, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err })
+            }
+            else {
+                res.send(result);
+                console.log(result);
+            }
+        });
+        client.close();
+    });
+});
+
+app.post('/updateStatus', (req, res) => {
+    const appointment = req.body;
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("patientsDetails").collection("appointments");
+        collection.updateOne({ _id: ObjectId(appointment.id) }, { $set: { "status": appointment.status } },
+            (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send({ message: err })
+                }
+                else {
+                    res.send(result);
+                    console.log(result);
+                }
+            });
         client.close();
     });
 });
